99edbf144f0a5eca75b6dd533d313af7b99e9a1c
Madhu Bhargav <madhubhargavp@gmail.com>
2019-03-03 19:12:25 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d580bb8880f3ce9f2b92c548f56475ea.js"
+  "/portfolio/precache-manifest.102e7fa8467a7dd9dbdc2566e65e7c71.js"
 );
 
 workbox.clientsClaim();
